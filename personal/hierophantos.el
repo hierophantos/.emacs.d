4aa4b68f843a056a7254b0aab97f1297ae683a15
Hierophantos <silvergodofgold@gmail.com>
2015-11-05 01:31:20 -0800

    key mapping shinanigans

--- personal/hierophantos.el
+++ personal/hierophantos.el
@@ -1,7 +1,6 @@
 ;;; My Personal Emacs Customizations
 
 ;;; My Key Bindings
-;;; Code:
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
 (global-set-key [C-tab] 'switch-to-next-buffer)
@@ -9,34 +8,59 @@
 (global-set-key [s-right] 'switch-to-next-buffer)
 (global-set-key [s-left] 'switch-to-prev-buffer)
 
+(global-set-key [s-right] 'switch-to-next-buffer)
+(global-set-key [C-M-u] 'switch-to-prev-buffer)
+
 (global-set-key [C-s-left] 'shrink-window-horizontally)
 (global-set-key [C-s-right] 'enlarge-window-horizontally)
 (global-set-key [C-s-down] 'shrink-window)
 (global-set-key [C-s-up] 'enlarge-window)
 
+;; (global-set-key [C-M-u] 'shrink-window-horizontally)
+;; (global-set-key [M-right] 'shrink-window-horizontally)
+;; (global-set-key [M-down] 'shrink-window)
+;; (global-set-key [M-up] 'enlarge-window) 
+
 (global-set-key (kbd "C-c C-u") 'cider-eval-print-last-sexp)
-;(global-set-key [C-c C-u] 'cider-eval-print-handler)
+;; (global-set-key [C-c C-u] 'cider-eval-print-handler)
 
 (global-set-key [C-s-o] 'project-explorer-mode)
 
+;; Command->meta, alt->super
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+
+;;; Mouse Input
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] (lambda ()
+                              (interactive)
+                              (scroll-down 1)))
+  (global-set-key [mouse-5] (lambda ()
+                              (interactive)
+                              (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t))
 
 ;;; My Modes
-(add-to-list 'load-path "/Users/Sophia/.emacs.d/elpa/newlisp-mode")
-(require 'newlisp-mode)
+;; (add-to-list 'load-path "/Users/Sophia/.emacs.d/elpa/newlisp-mode")
+;; (require 'newlisp-mode)
 
 (require 'golden-ratio)
-;(golden-ratio-mode nil)
+;; (golden-ratio-mode nil)
 
 (defun disable-guru-mode ()
   (guru-mode -1))
 
 (add-hook 'prelude-prog-mode-hook 'disable-guru-mode t)
 
-(scroll-bar-mode -1)
+;; (scroll-bar-mode -1)
 
 ;; turn off whitespace mode
-;(add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
+(add-hook 'prog-mode-hook 'prelude-turn-off-whitespace t)
 (setq prelude-whitespace nil)
+
 
 
 
